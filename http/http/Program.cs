a3140c3a2a3d5ab775f5289ba488cad3418c925f
Farxodbek Madrahimov <110379042+FarxodbekMadrahimov@users.noreply.github.com>
2023-10-04 21:00:47 +0500

    Add files via upload

---
+++ http/http/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System;
+
+
+
+using (HttpClient client = new HttpClient())
+{
+    string base_url = "https://jsonplaceholder.typicode.com/posts";
+    HttpResponseMessage response = await client.GetAsync(base_url);
+    string respon = await response.Content.ReadAsStringAsync();
+    //Console.WriteLine(respon);
+    var roots = JsonConvert.DeserializeObject<List<root>>(respon);
+    foreach (var root in roots)
+    {
+        Console.WriteLine($"userId - {root.userId}");
+       
+        Console.WriteLine($"ID - {root.Id}");
+        Console.WriteLine($"title - {root.title}");
+        Console.WriteLine($"body - {root.body}");
+        Console.WriteLine("  ");
+    }
+}
+public class root
+{
+    public int userId { get; set; }
+    public int Id { get; set; }
+    public string title { get; set; }
+    public string body { get; set; }
+}
